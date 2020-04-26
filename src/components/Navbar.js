9313c9fb7532e2616c1cf17651d79d26b5a6e8a1
BadCoder1337 <anton.tiz.musin@gmail.com>
2020-04-27 02:54:07 +0300

    Mobile navbar fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -38,7 +38,8 @@ const Navbar = class extends React.Component {
             <div
               className={`navbar-burger burger ${this.state.navBarActiveClass}`}
               data-target="navMenu"
-              onClick={() => this.toggleHamburger()}
+              onClick={this.toggleHamburger}
+              onKeyDown={this.toggleHamburger}
             >
               <span />
               <span />
