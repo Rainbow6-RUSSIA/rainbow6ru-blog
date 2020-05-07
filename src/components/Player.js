87825f82a417eff1571ac050372e45ad266c7c3e
BadCoder1337 <anton.tiz.musin@gmail.com>
2020-05-07 13:23:49 +0300

    Remove Mixer iframe border

--- src/components/Player.js
+++ src/components/Player.js
@@ -5,7 +5,7 @@ import TwitchPlayer from 'react-twitch-embed-video'
 const Player = ({ platform, name }) => {
     const EmbedPlayer = 
         platform === 'twitch' ? ({ ...args }) => <TwitchPlayer layout="video" targetId={`tw-${args.channel.toLowerCase()}`} {...args}/> :
-        platform === 'mixer' ? ({ ...args }) => <MixerPlayer {...args}/> : null
+        platform === 'mixer' ? ({ ...args }) => <MixerPlayer style={{ border: 'none' }} {...args}/> : null
     return <EmbedPlayer muted height={180} width={320} channel={name} />
 }
 
